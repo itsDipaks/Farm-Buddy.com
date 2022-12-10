d4c6cffd9615c15bd25d1168292b7c970feacd4e
Dipak Pawar <itsdipakpawar4206@gmail.com>
2022-12-10 13:21:57 +0530

    added Frontend React Template

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FarmEasy.in</h1>
+    </div>
+  );
+}
+
+export default App;
