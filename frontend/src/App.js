843f0f7b377ebb286fd78a41d2787e34a1ae1365
sachin9977 <sc09626@gmail.com>
2022-12-13 21:30:23 +0530

    Login-Navbar-PincodeDrawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import AdminLogin from './Components/Admin/AdminLogin';
 import AdminSign from './Components/Admin/AdminSign';
 import Dashboard from './Pages/AdminPages/Dashboard';
+import Navbar from './Pages/Navbar/Navbar';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <>
-      <h1>FarmEasy.in</h1>
+      {/* <h1>FarmEasy.in</h1> */}
+      <Navbar/>
     {/* <AdminSign/> */}
     {/* <AdminLogin/> */}
-    <Dashboard/>
+    {/* <Dashboard/> */}
     <div className="App">
     </div>
     </>
