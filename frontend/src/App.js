93091412ada305ba6e76b83123b5116297a46fd1
Ravi <63177572+Ravi80595@users.noreply.github.com>
2022-12-14 15:48:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Pages/Navbar/Navbar';
-import AllRoutes from './Components/Routes/AllRoutes';
+import AllRoutes from './Components/Routes/Allroutes';
 
 function App() {
   return (
