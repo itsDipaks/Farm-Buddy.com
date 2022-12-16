ec16c8008485f403c26b6722670023fa7674636c
Anand Waghmare <103956874+anand6840@users.noreply.github.com>
2022-12-16 23:59:13 +0530

    Brands and DayDeals Added

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -3,10 +3,15 @@ import ShopByCategories from "./Category/ShopByCategories"
 
 import Footer from "./Footer/footer"
 import Offers from "./Offers/Offers";
+import Brands from "./Brands/Brands";
+import DayDeals from './components/DayDeals/DayDeals';
+
 
 function Home() {
   return (
     <div >
+    <Brands/>
+    <DayDeals/>
     <ShopByCategories/>
      <Offers/>
      <Footer/>
