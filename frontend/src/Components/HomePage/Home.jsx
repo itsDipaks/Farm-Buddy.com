bf3789da44cfcb39980d3def42ee93fcc6e3b7e6
Anand Waghmare <103956874+anand6840@users.noreply.github.com>
2022-12-17 01:46:11 +0530

    Banner Linked with Home

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -5,12 +5,14 @@ import Footer from "./Footer/footer"
 import Offers from "./Offers/Offers";
 import Brands from "./Brands/Brands";
 import DayDeals from './components/DayDeals/DayDeals';
+import OffersBanner from "./OffersBanner/OffersBanner";
 
 
 function Home() {
   return (
     <div >
     <Brands/>
+    <OffersBanner/>
     <DayDeals/>
     <ShopByCategories/>
      <Offers/>
