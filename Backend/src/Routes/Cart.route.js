9b99b09036443c0bf074da050a6bc964a0bb6ea4
Dipak Pawar <itsdipakpawar4206@gmail.com>
2022-12-17 23:52:48 +0530

    Add some backend

--- Backend/src/Routes/Cart.route.js
+++ Backend/src/Routes/Cart.route.js
@@ -5,11 +5,13 @@ const { UserAuthenticate } = require("../middelwares/UserAuthenticate")
 
 const CartRouter=Router()
 
-CartRouter.post("/addtocart",CartController.Addtocart)
+CartRouter.post("/addtocart",UserAuthenticate,CartController.Addtocart)
 
 CartRouter.delete("/delproduct/:prodid",UserAuthenticate)
 
 CartRouter.patch("/edite/:prodid",UserAuthenticate)
 
+CartRouter.get("/usercart",UserAuthenticate,CartController.GetUsersCart)
 
 module.exports={CartRouter}
+
--- Backend/src/middelwares/UserAuthenticate.js
+++ Backend/src/middelwares/UserAuthenticate.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
 const UserAuthenticate = (req,res,next)=>{
-    const token = req.headers?.token
+    const token = req.headers.token
     if(token){
-        const decoded = jwt.verify(token,process.env.PRIVATEKEY)
-        if(decoded){
-            const user_id  = decoded.user_id
-            req.body.user_id = user_id
-            next()
-        }
-        else{
-            res.send("Please Login again")
-        }
+        jwt.verify(token,process.env.PRIVATEKEY, function(err, decoded) {
+            if(decoded){
+                const user_id  = decoded.user_id
+                req.body.user_id = user_id
+                next()
+            }
+            else{
+                res.send({msg:"Please Login again",err:err})
+            }
+        });
     }
     else{
         res.send('Please Login token not found')
